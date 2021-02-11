4dbce258219963a3da8b9e82e12df211df9d0a54
venkyjscript <venkatesh.mamidipelli@gmail.com>
2021-02-11 04:18:04 +0000

    first class added

---
+++ foo.py
@@ -0,0 +1,8 @@
+class Human:
+    name=""
+    age=0
+    weight=0.0
+    #def __init__(self):
+
+    def showDetails(self,obj):
+        return "Hello "+obj.name+" your age= "+self.age+" and weight ="+self.weight
